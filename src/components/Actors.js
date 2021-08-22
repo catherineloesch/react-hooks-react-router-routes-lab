9911e556223b435b6bee829fe9fb450aad145449
Catherine Loesch <catherine.loesch@protonmail.com>
2021-08-22 03:25:43 +0100

    assignment completed

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,28 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+
+  const actorElements = actors.map((actor) => {
+    
+    return (
+      <div key={actor.name}>
+        <h3>Name: {actor.name}</h3>
+        movies:
+        <ul>
+          {actor.movies.map((movie) => {
+            return (<li key={movie}>
+              {movie}
+              </li>)
+          })}
+        </ul>
+      </div>
+    )
+  })
+
+  return (<div>
+      <h1>Actors Page</h1>
+      {actorElements}
+    </div>);
 }
 
 export default Actors;
